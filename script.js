6adc45675e1d523b60ff2b87ab49cdd1e540bd94
Pasinduroshan <roshanpasindu@gmail.com>
2025-02-16 20:43:32 +0530

    added new changes, new navbar options ,responsie

---
+++ script.js
@@ -0,0 +1,11 @@
+// Show Sidebar
+function showSidebar() {
+    document.querySelector('.sidebar').classList.add('active');
+    
+}
+
+// Close Sidebar
+function closeSidebar() {
+    document.querySelector('.sidebar').classList.remove('active');
+    
+}
